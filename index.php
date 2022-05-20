a9fb0311fd7b91b2a56c16de0646b187fd0a1055
1306ThanhHien <105936469+1306ThanhHien@users.noreply.github.com>
2022-05-21 00:12:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bank</title>
+    <link rel="stylesheet" href="./assets/style.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+
+
+</head>
+<body>
+
+    <!-- start header -->
+    <header>
+        <a href="#" class="logo"><img src="img/tdt-logo.png" alt="logo"></i></a>
+        <div id="menu-bars"  class="fa-solid fa-bars"></div>
+        <nav class="navbar">
+            <a href="#home">Home</a>
+            <a href="#service">Our Services</a>
+            <a href="#fetures">Our Fetures</a>
+            <a href="#review">Review</a>
+            <a href="#contact">Contact</a>
+            <a href="./user/login.php">Login</a>
+
+        </nav>
+    </header>
+    <!-- end header -->
+
+    <!-- start section -->
+     <section class="home" id="home">
+        <div class="content">
+            <h3>Make your online banking experience wonderful</h3>
+            <p>lorem ipsum dolor sit amet, consectetur adipisicing</p>
+            <a href="./user/login.php" class="btn">Login</a>
+            <a href="./user/register.php" class="btn">Get started</a>
+
+        </div>
+
+        <div class="image">
+            <img src="img/Bank-img1.svg" alt="hambager">
+        </div>
+     </section>   
+    <!--end section -->
+
+
+
+    <!-- Specialy -->
+     <section id="service" class="specialy">
+         <h1 class="heading">Our <span>Services</span></h1>
+         <div class="box-container">
+             <div class="box">
+                 <img class="image" src="img/security.png" alt="" >
+                 <div class="content">
+                     <img src="img/service1.jpg" alt="">
+                     <h3>Secure Payment</h3>
+                     <p>The Security of Customer is the Number One Priority for us.</p>
+                 </div>
+             </div>
+
+             <div class="box">
+                <img class="image" src="img/card1.png" alt="" >
+                <div class="content">
+                    <img src="img/service2.png" alt="">
+                    <h3>Credit & Debit Cards</h3>
+                    <p>Manage your credit card and debit card activity with us, without any problems.</p>
+                </div>
+            </div>
+
+            <div class="box">
+                <img class="image" src="img/banking.png" alt="" >
+                <div class="content">
+                    <img src="img/service3.png" alt="">
+                    <h3>Online Banking</h3>
+                    <p>Our Bank allows there customer to access their account 24/7 without having to visit a physical branch with the help of online banking.</p>
+                </div>
+            </div>
+
+            <div class="box">
+                <img class="image" src="img/isua.png" alt="" >
+                <div class="content">
+                    <img src="img/service4.png" alt="">
+                    <h3>Insurance</h3>
+                    <p>Secure your and your family’s future with our various insurance plans and services.</p>
+                </div>
+            </div>
+
+            <div class="box">
+                <img class="image" src="img/ser.png" alt="" >
+                <div class="content">
+                    <img src="img/service5.jpg" alt="">
+                    <h3>24 x 7 Service</h3>
+                    <p>Our team would love to solve your problems at any instance of the day.</p>
+                </div>
+            </div>
+
+            <div class="box">
+                <img class="image" src="img/loans.png" alt="" >
+                <div class="content">
+                    <img src="img/service6.png" alt="">
+                    <h3>Loans</h3>
+                    <p>Loans approved with the digital documents and digital signature.</p>
+                </div>
+            </div>
+
+
+         </div>
+     </section>
+
+    <h1 class="heading">Our <span>Fetures</span></h1>
+
+    <section class="fetures" id="fetures">
+        <div class="content">
+            <div class="group-features">
+               <h4>Secure Pay</h4>
+              <p>We provide the feature of End to end encrypted details while the ongoing payment.</p>
+            </div>
+            <div class="group-features">
+               <h4>Graphical Dashboard</h4>
+               <p>We provide easy to use and flexible dashboard to our customer with visualize graphs.</p>
+            </div>
+            <div class="group-features">
+               <h4>2 Step Verification</h4>
+              <p>We add an extra layer of security to your account in case your password is stolen.</p>
+            </div>
+            <div class="group-features">
+               <h4>Instant Message Alert</h4>
+              <p>We Provide an instant payment alert message after every transaction perfrom with your account.</p>
+            </div>
+              
+        </div>
+
+        <div class="image">
+            <img src="img/features.svg" alt="">
+        </div>
+    </section>
+
+
+    
+
+
+     <!-- Review -->
+     <section id="review" class="review">
+         <h1 class="heading">Our Customer <span>Reviews</span> </h1>
+         <div class="box-container">
+             <div class="box">
+                 <img src="img/pic1.png" alt="">
+                 <h3>john deo</h3>
+                 <div class="stars">
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                    <i class="far fa-star"></i>
+                 </div>
+                 <p>The Transaction system of the TDT bank is very fast. We can easily manage our transaction history
+                  with digramatical manner.</p>    
+             </div>
+
+
+             <div class="box">
+                <img src="img/pic2.png" alt="">
+                <h3>Ibra</h3>
+                <div class="stars">
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                </div>
+                <p>This is the wonderful bank i ever seen. You can esily create account in just 10 minutes
+                  and start your online banking. Thank You TDT Bank!</p>    
+            </div>
+
+            <div class="box">
+                <img src="img/pic3.png" alt="">
+                <h3>Sasuke</h3>
+                <div class="stars">
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                   <i class="far fa-star"></i>
+                </div>
+                <p>This Bank Gives instant alert after every transaction. Personnaly I like the UI of
+                  dashboard and profile section.</p>    
+            </div>
+         </div>
+     </section>
+
+     <!-- contact -->
+     <section class="contact" id="contact">
+         <h1 class="heading"> <span>Contact</span> us</h1>
+         <div class="row">
+             <div class="image">
+                 <img src="img/contact.jpg" alt="">
+             </div>
+
+             <form action="">
+                 <div class="inputBox">
+                     <input type="text" placeholder="name">
+                     <input type="email" placeholder="email">
+
+                 </div>
+                 <div class="inputBox">
+                    <input type="number" placeholder="number">
+                    <!-- <input type="text" placeholder="food name"> -->
+
+                </div>
+
+                <textarea placeholder="Message" name="" id="" cols="30" rows="10"></textarea>
+                <input type="submit" value="Contact now" class="btn">
+
+             </form>
+         </div>
+     </section>
+    
+
+     <!-- footer -->
+     <section class="footer" id="footer">
+         <div class="share">
+            <a href="#" class="btn">facebook</a>
+            <a href="#" class="btn">twitter</a>
+            <a href="#" class="btn">instagram</a>
+            <a href="#" class="btn">pinterest</a>
+            <a href="#" class="btn">linkedin</a>
+         </div>
+     </section>
+
+
+    <!--  statr script -->
+    <script src="./assets/script.js"></script>
+    <!-- end script -->
+</body>
+</html>
